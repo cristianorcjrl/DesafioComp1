1f4808b99942a7071cd13719ff482db826c691a2
Cristian Ramirez <cristianoramirez@gmail.com>
2022-06-26 22:34:36 -0500

    desafio complementario 1

---
+++ ciclos/js/script.js
@@ -0,0 +1,50 @@
+let repetir = true
+while(repetir){
+let numeroRomano = prompt("INGRESE UN NUMERO ROMANO (DEL 1 AL 20)").toUpperCase()
+if (numeroRomano == "I") {
+    alert("1");
+}else if (numeroRomano == "II") {
+    alert("SU NÚMERO ENTERO ES: 2");
+}else if (numeroRomano == "III") {
+    alert("SU NÚMERO ENTERO ES: 3");
+}else if (numeroRomano == "IV") {
+    alert("SU NÚMERO ENTERO ES: 4");
+}else if (numeroRomano == "V") {
+    alert("SU NÚMERO ENTERO ES: 5");
+}else if (numeroRomano == "VI") {
+    alert("SU NÚMERO ENTERO ES: 6");
+}else if (numeroRomano == "VII") {
+    alert("SU NÚMERO ENTERO ES: 7");
+}else if (numeroRomano == "VIII") {
+    alert("SU NÚMERO ENTERO ES: 8");
+}else if (numeroRomano == "IX") {
+    alert("SU NÚMERO ENTERO ES: 9");
+}else if (numeroRomano == "X") {
+    alert("SU NÚMERO ENTERO ES: 10");
+}else if (numeroRomano == "XI") {
+    alert("SU NÚMERO ENTERO ES: 11");
+}else if (numeroRomano == "XII") {
+    alert("SU NÚMERO ENTERO ES: 12");
+}else if (numeroRomano == "XIII") {
+    alert("SU NÚMERO ENTERO ES: 13");
+}else if (numeroRomano == "XIV") {
+    alert("SU NÚMERO ENTERO ES: 14");
+}else if (numeroRomano == "XV") {
+    alert("SU NÚMERO ENTERO ES: 15");
+}else if (numeroRomano == "XVI") {
+    alert("SU NÚMERO ENTERO ES: 16");
+}else if (numeroRomano == "XVII") {
+    alert("SU NÚMERO ENTERO ES: 17");
+}else if (numeroRomano == "XVIII") {
+    alert("SU NÚMERO ENTERO ES: 18");
+}else if (numeroRomano == "XIX") {
+    alert("SU NÚMERO ENTERO ES: 19");
+}else if (numeroRomano == "XX") {
+    alert("SU NÚMERO ENTERO ES: 20");
+}
+else {
+    alert("NÚMERO EQUIVOCADO, INGRESE DE NUEVO"); 
+}
+console.log(numeroRomano)
+
+}
